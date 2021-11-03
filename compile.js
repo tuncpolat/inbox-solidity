23ae0b43a62141d96bac772bfd9d9b22e097da18
Tunc Polat <tunc.polat@simplweb.ch>
2021-11-03 15:24:54 +0100

    Write first test

--- compile.js
+++ compile.js
@@ -5,5 +5,6 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-// compile statements for
+// compile statement
+// Inbox Object returned from compile - includes bytecode & ABI (interface - needed to access the bytecode through Web3)
 module.exports = solc.compile(source, 1).contracts[":Inbox"];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,33 @@
-const assert = require("assert");
+// we need to be aware that we are dealing with money, so testing is important (with mocha)
+// we are going to deploy our contract to a local test network (TestRPC/Ganache)
+
+const assert = require("assert"); // part of node
 const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const Web3 = require("web3"); // constructor function (Obj)
+const { interface, bytecode } = require("../compile");
+
+// instance of Web3; Provider as communication layer between Web3 library and a Ethereum Network (e.g. Ganache)
+// send and receive messages between Web3 and Ganache
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // get a list of all unlocked accounts from ganache
+  accounts = await web3.eth.getAccounts();
+
+  // use one of those accounts to deploy the contract (with web3 library)
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
